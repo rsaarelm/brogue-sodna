00e7cf696faa35aedbf7a8338818cf20c264527b
Risto Saarelma <risto.saarelma@iki.fi>
2014-11-16 17:10:30 +0200

    Sodna supports keymapping

--- src/platform/sodna-platform.c
+++ src/platform/sodna-platform.c
@@ -3,6 +3,7 @@
 #include "platform.h"
 #include "sodna_util.h"
 #include <time.h>
+#include <string.h>
 
 extern playerCharacter rogue;
 
@@ -20,6 +21,80 @@ static int old_mouse_y = 0;
 static int mouse_x = 0;
 static int mouse_y = 0;
 #define PAUSE_BETWEEN_EVENT_POLLING 36
+
+// Mappings from unprintable keys to unprintable or printable (negative value)
+// indices. Zero means no mapping.
+static int unprintable_mappings[128] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+};
+
+// Mappings from printable keys to printable or unprintable (negative value)
+// indices. Zero means no mapping.
+static int printable_mappings[128] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+};
+
+static struct { const char* name; int sodna_id; } sodna_key_names[] = {
+    { "ESCAPE", SODNA_KEY_ESCAPE },
+    { "BACKSPACE", SODNA_KEY_BACKSPACE },
+    { "TAB", SODNA_KEY_TAB },
+    { "ENTER", SODNA_KEY_ENTER },
+    { "PAGEUP", SODNA_KEY_PAGE_UP },
+    { "PAGEDOWN", SODNA_KEY_PAGE_DOWN },
+    { "END", SODNA_KEY_END },
+    { "HOME", SODNA_KEY_HOME },
+    { "UP", SODNA_KEY_UP },
+    { "LEFT", SODNA_KEY_LEFT },
+    { "RIGHT", SODNA_KEY_RIGHT },
+    { "DOWN", SODNA_KEY_DOWN },
+    { "PRINTSCREEN", SODNA_KEY_PRINT_SCREEN },
+    { "PAUSE", SODNA_KEY_PAUSE },
+    { "INSERT", SODNA_KEY_INSERT },
+    { "DELETE", SODNA_KEY_DELETE },
+    { "KP0", SODNA_KEY_KP_0 },
+    { "KP1", SODNA_KEY_KP_1 },
+    { "KP2", SODNA_KEY_KP_2 },
+    { "KP3", SODNA_KEY_KP_3 },
+    { "KP4", SODNA_KEY_KP_4 },
+    { "KP5", SODNA_KEY_KP_5 },
+    { "KP6", SODNA_KEY_KP_6 },
+    { "KP7", SODNA_KEY_KP_7 },
+    { "KP8", SODNA_KEY_KP_8 },
+    { "KP9", SODNA_KEY_KP_9 },
+    { "KPENTER", SODNA_KEY_KP_ENTER },
+    { "KPDEC", SODNA_KEY_KP_DECIMAL },
+    { "KPDIV", SODNA_KEY_KP_DIVIDE },
+    { "KPMUL", SODNA_KEY_KP_MULTIPLY },
+    { "KPSUB", SODNA_KEY_KP_MINUS },
+    { "KPADD", SODNA_KEY_KP_PLUS },
+    { "F1", SODNA_KEY_F1 },
+    { "F2", SODNA_KEY_F2 },
+    { "F3", SODNA_KEY_F3 },
+    { "F4", SODNA_KEY_F4 },
+    { "F5", SODNA_KEY_F5 },
+    { "F6", SODNA_KEY_F6 },
+    { "F7", SODNA_KEY_F7 },
+    { "F8", SODNA_KEY_F8 },
+    { "F9", SODNA_KEY_F9 },
+    { "F10", SODNA_KEY_F10 },
+    { "F11", SODNA_KEY_F11 },
+    { "F12", SODNA_KEY_F11 },
+    { NULL, 0 }
+};
 
 static boolean mouse_pos_changed() {
     if (old_mouse_x != mouse_x || old_mouse_y != mouse_y) {
@@ -178,6 +253,26 @@ static void sodna_nextKeyOrMouseEvent(
             }
         }
 
+        // Keymap translation.
+        if (e.type == SODNA_EVENT_CHARACTER && e.ch.code < 128) {
+            int km = printable_mappings[e.ch.code];
+            if (km > 0) {
+                e.ch.code = km;
+            } else if (km < 0) {
+                e.type = SODNA_EVENT_KEY_DOWN;
+                e.key.layout = -km;
+            }
+        } else if (e.type == SODNA_EVENT_KEY_DOWN && e.key.layout < 128) {
+            int km = unprintable_mappings[e.key.layout];
+            if (km > 0) {
+                e.key.layout = km;
+            } else if (km < 0) {
+                e.type = SODNA_EVENT_CHARACTER;
+                e.ch.code = -km;
+            }
+        }
+
+        // Back to event handling.
         if (e.type == SODNA_EVENT_CHARACTER) {
             returnEvent->param1 = e.ch.code;
             returnEvent->eventType = KEYSTROKE;
@@ -190,28 +285,28 @@ static void sodna_nextKeyOrMouseEvent(
             switch (e.key.layout) {
 #define K(sodna, brogue) case sodna: returnEvent->param1 = brogue; return;
                 K(SODNA_KEY_UP, UP_ARROW)
-                    K(SODNA_KEY_DOWN, DOWN_ARROW)
-                    K(SODNA_KEY_LEFT, LEFT_ARROW)
-                    K(SODNA_KEY_RIGHT, RIGHT_ARROW)
-                    K(SODNA_KEY_HOME, UPLEFT_KEY)
-                    K(SODNA_KEY_END, DOWNLEFT_KEY)
-                    K(SODNA_KEY_PAGE_UP, UPRIGHT_KEY)
-                    K(SODNA_KEY_PAGE_DOWN, DOWNRIGHT_KEY)
-                    K(SODNA_KEY_KP_1, NUMPAD_1)
-                    K(SODNA_KEY_KP_2, NUMPAD_2)
-                    K(SODNA_KEY_KP_3, NUMPAD_3)
-                    K(SODNA_KEY_KP_4, NUMPAD_4)
-                    K(SODNA_KEY_KP_5, NUMPAD_5)
-                    K(SODNA_KEY_KP_6, NUMPAD_6)
-                    K(SODNA_KEY_KP_7, NUMPAD_7)
-                    K(SODNA_KEY_KP_8, NUMPAD_8)
-                    K(SODNA_KEY_KP_9, NUMPAD_9)
-                    K(SODNA_KEY_KP_0, NUMPAD_0)
-                    K(SODNA_KEY_TAB, TAB_KEY)
-                    K(SODNA_KEY_ENTER, RETURN_KEY)
-                    K(SODNA_KEY_KP_ENTER, ENTER_KEY)
-                    K(SODNA_KEY_BACKSPACE, DELETE_KEY)
-                    K(SODNA_KEY_ESCAPE, ESCAPE_KEY)
+                K(SODNA_KEY_DOWN, DOWN_ARROW)
+                K(SODNA_KEY_LEFT, LEFT_ARROW)
+                K(SODNA_KEY_RIGHT, RIGHT_ARROW)
+                K(SODNA_KEY_HOME, UPLEFT_KEY)
+                K(SODNA_KEY_END, DOWNLEFT_KEY)
+                K(SODNA_KEY_PAGE_UP, UPRIGHT_KEY)
+                K(SODNA_KEY_PAGE_DOWN, DOWNRIGHT_KEY)
+                K(SODNA_KEY_KP_1, NUMPAD_1)
+                K(SODNA_KEY_KP_2, NUMPAD_2)
+                K(SODNA_KEY_KP_3, NUMPAD_3)
+                K(SODNA_KEY_KP_4, NUMPAD_4)
+                K(SODNA_KEY_KP_5, NUMPAD_5)
+                K(SODNA_KEY_KP_6, NUMPAD_6)
+                K(SODNA_KEY_KP_7, NUMPAD_7)
+                K(SODNA_KEY_KP_8, NUMPAD_8)
+                K(SODNA_KEY_KP_9, NUMPAD_9)
+                K(SODNA_KEY_KP_0, NUMPAD_0)
+                K(SODNA_KEY_TAB, TAB_KEY)
+                K(SODNA_KEY_ENTER, RETURN_KEY)
+                K(SODNA_KEY_KP_ENTER, ENTER_KEY)
+                K(SODNA_KEY_BACKSPACE, DELETE_KEY)
+                K(SODNA_KEY_ESCAPE, ESCAPE_KEY)
 #undef K
                     break;
             }
@@ -285,8 +380,51 @@ static void sodna_plotChar(
         sodna_cells()[xLoc + COLS * yLoc] = c;
 }
 
+// out_is_printable: True if the input is a printable character and goes to the printable mappings table.
+// out_index: The index in either the printable or nonprintable mappings table.
+// return: True if a mapping was found, false if not.
+static boolean resolve_input_name(boolean* out_is_printable, int* out_index, const char *input_name) {
+    if (strlen(input_name) == 1 && input_name[0] >= '!' && input_name[0] <= '~') {
+        // Printable ASCII.
+        *out_is_printable = true;
+        *out_index = input_name[0];
+        return true;
+    }
+
+    // Special case for SPACE, you can't distinguish the char so it has to be named.
+    if (strcmp(input_name, "SPACE") == 0) {
+        *out_is_printable = true;
+        *out_index = ' ';
+        return true;
+    }
+
+    int i = 0;
+    while (sodna_key_names[i].name) {
+        if (strcmp(input_name, sodna_key_names[i].name) == 0) {
+            *out_is_printable = false;
+            *out_index = sodna_key_names[i].sodna_id;
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
+
 static void sodna_remap(const char *input_name, const char *output_name) {
-    // TODO
+    int in_idx;
+    boolean in_printable;
+
+    int out_idx;
+    boolean out_printable;
+
+    if (!resolve_input_name(&in_printable, &in_idx, input_name)) return;
+    if (!resolve_input_name(&out_printable, &out_idx, output_name)) return;
+
+    // Use negative numbers to signal table switch.
+    out_idx *= (in_printable == out_printable ? 1 : -1);
+
+    if (in_printable) printable_mappings[in_idx] = out_idx;
+    else unprintable_mappings[in_idx] = out_idx;
 }
 
 static boolean modifier_held(int modifier) {
